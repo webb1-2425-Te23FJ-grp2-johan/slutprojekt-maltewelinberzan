be88fd2045ca786e3ee0ff91dc73e476cb1409d2
maltewelinberzan <malwel053@edu.linkoping.se>
2025-05-22 12:15:00 +0000

    Klar

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,22 +1,21 @@
- const menuButton = document.getElementById('menu-button');
-        const menu = document.getElementById('menu');
-        const menuCloseButton = document.getElementById('menu-close');
-        const body = document.querySelector('body');
+const menuButton = document.getElementById("menu-button");
+const menu = document.getElementById("menu");
+const menuCloseButton = document.getElementById("menu-close");
+const body = document.querySelector("body");
 
-        menuButton.addEventListener('click', () => {
-            menu.classList.add('menu-open');
-            body.classList.add('overflow-hidden');
-            console.log("hejehejehej")
-        });
+menuButton.addEventListener("click", () => {
+  menu.classList.add("menu-open");
+  body.classList.add("overflow-hidden");
+});
 
-        menuCloseButton.addEventListener('click', () => {
-            menu.classList.remove('menu-open');
-            body.classList.remove('overflow-hidden');
-        });
+menuCloseButton.addEventListener("click", () => {
+  menu.classList.remove("menu-open");
+  body.classList.remove("overflow-hidden");
+});
 
-        document.addEventListener('click', (event) => {
-            if (!menu.contains(event.target) && !menuButton.contains(event.target)) {
-                menu.classList.remove('menu-open');
-                body.classList.remove('overflow-hidden');
-            }
-        });
+document.addEventListener("click", (event) => {
+  if (!menu.contains(event.target) && !menuButton.contains(event.target)) {
+    menu.classList.remove("menu-open");
+    body.classList.remove("overflow-hidden");
+  }
+});
